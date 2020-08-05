b05c7c594a4422ab9f97211e2f8c6d254dca386d
Arvind Kumar <arvind.11810214@lpu.in>
2020-08-05 13:08:32 +0530

    two libraries imported and debug checked how it works the lifecycle

--- unitOne/app/src/main/java/com/example/unitone/MainActivity.java
+++ unitOne/app/src/main/java/com/example/unitone/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.unitone;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle", "onStart invoked");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle", "onResume invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle", "on Pause Revoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+Log.d("lifecycle", "onRestart invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle", "on Stop invoked");}
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle", "onDestroy invoked");
     }
 }
