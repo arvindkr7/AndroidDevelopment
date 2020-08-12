6b10894893f5ebadfffe5e4c1fade7a1ce006f90
Arvind Kumar <arvind.11810214@lpu.in>
2020-08-12 14:42:57 +0530

    toast added on button

--- unitOne/app/src/main/java/com/example/unitone/P2LinearLayoutDemo.java
+++ unitOne/app/src/main/java/com/example/unitone/P2LinearLayoutDemo.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class P2LinearLayoutDemo extends AppCompatActivity implements View.OnClickListener {
     Button clearbtn, submitbtn, cancelledbtn;
@@ -36,19 +37,45 @@ public class P2LinearLayoutDemo extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view) {
+        String userName = name.getText().toString();
+        String regnum=regno.getText().toString();
+
         switch (view.getId()){
             case R.id.ClearButton:
                 //System.out.println("Clear Button clicked");
-                name.getText().clear();
-                regno.setText(""); // we can use any of these method to clear input texts
+                // we can use any of these method to clear input texts
+                if (userName.isEmpty() && regnum.isEmpty()){
+                    toast("Nothing to clear");
+                }
+                else if (!userName.isEmpty() && !regnum.isEmpty()){
+                    name.getText().clear();
+                    regno.setText("");
+                    toast("Cleared");
+                }
+                else if (!userName.isEmpty()){
+                    name.getText().clear();
+                toast("Name Cleared");}
+                else if(!regnum.isEmpty()){
+                    regno.setText("");
+                    toast("Registration Number cleared");
+                }
                 break;
 
             case R.id.SubmitButton:
                 System.out.println("Submit Button clicked");
+
+                if (userName.isEmpty() || regnum.isEmpty()){
+                    toast("Provide all the required details above");
+                }else{
+                toast("Submitted successfully");}
                 break;
             case R.id.CancelledButton:
                 System.out.println("Cancelled Button clicked");
+                toast("Cancelled");
                 break;
         }
+    }
+    public void toast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
