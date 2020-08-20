f034070b4ea38a0d56eec60689c2d18a626107b2
Arvind Kumar <arvind.11810214@lpu.in>
2020-08-20 14:20:10 +0530

    SignUp Form feature added on click event

--- unitOne/app/src/main/java/com/example/unitone/SignUp.java
+++ unitOne/app/src/main/java/com/example/unitone/SignUp.java
@@ -1,15 +1,22 @@
 package com.example.unitone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static com.example.unitone.R.*;
 
@@ -21,16 +28,46 @@ public class SignUp extends AppCompatActivity {
         setContentView(layout.activity_sign_up);
 
         TextView textView= findViewById(id.statement);
-        String text = "I accept the Terms of Use & Privacy Policy";
+        String statement = "I accept the Terms of Use & Privacy Policy";
+
+        SpannableString ss= new SpannableString(statement);
+
+        ClickableSpan span1= new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Toast.makeText(SignUp.this, "You read Terms of Use", Toast.LENGTH_SHORT).show();
+            }
+
+        };
+
+        ClickableSpan span2 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Toast.makeText(SignUp.this, "You read Privacy Policy", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        ss.setSpan(span1, statement.indexOf("Terms"), statement.indexOf("se")+2,  Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(span2, statement.indexOf("Privacy"), statement.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(ss);
+
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+/*
 
         SpannableStringBuilder ssb = new SpannableStringBuilder(text);
 
         ForegroundColorSpan fcs= new ForegroundColorSpan(getResources().getColor(color.skyBlue));
+        ForegroundColorSpan fcs2= new ForegroundColorSpan(getResources().getColor(color.skyBlue));
 
-        ssb.setSpan(fcs, 13,25, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
-        ssb.setSpan(fcs, 28, 42, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(fcs, text.indexOf("Terms"),text.indexOf("se")+2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(fcs2, text.indexOf("Pri"), text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         textView.setText(ssb);
+
+ */
 
 
     }
