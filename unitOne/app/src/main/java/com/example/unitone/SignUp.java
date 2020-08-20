3904b5ceb386e2e060acb1894409a5db5cf62c4b
Arvind Kumar <arvind.11810214@lpu.in>
2020-08-20 13:26:55 +0530

    SignUp Form minor issue fixed with first and last name views' width

--- unitOne/app/src/main/java/com/example/unitone/SignUp.java
+++ unitOne/app/src/main/java/com/example/unitone/SignUp.java
@@ -3,12 +3,35 @@ package com.example.unitone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import static com.example.unitone.R.*;
 
 public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(layout.activity_sign_up);
+
+        TextView textView= findViewById(id.statement);
+        String text = "I accept the Terms of Use & Privacy Policy";
+
+        SpannableStringBuilder ssb = new SpannableStringBuilder(text);
+
+        ForegroundColorSpan fcs= new ForegroundColorSpan(getResources().getColor(color.skyBlue));
+
+        ssb.setSpan(fcs, 13,25, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        ssb.setSpan(fcs, 28, 42, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(ssb);
+
+
     }
 }
