0c2ef5360401fd4d5fbf6e59b11f6f14f973c4b1
Arvind Kumar <arvind.11810214@lpu.in>
2020-10-19 12:58:49 +0530

    added birthday guess activity in Unit 5

---
+++ unit4/app/src/main/java/com/example/unit4/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.unit4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent i=getIntent();
+        String msg=i.getStringExtra("name");
+        Toast.makeText(this,msg, Toast.LENGTH_SHORT).show();
+    }
+
+}
