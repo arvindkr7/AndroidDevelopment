79b6365b8b9165686e94216db8fa9596e363980f
Arvind Kumar <arvind.11810214@lpu.in>
2021-01-12 23:11:19 +0530

    searchview and listview implemented. perfectly working

--- cse225Practicals/app/src/main/java/com/example/cse225practicals/P3Toolbar.java
+++ cse225Practicals/app/src/main/java/com/example/cse225practicals/P3Toolbar.java
@@ -1,30 +1,57 @@
 package com.example.cse225practicals;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-public class P3Toolbar extends AppCompatActivity {
+    public class P3Toolbar extends AppCompatActivity {
+        ArrayAdapter ad;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_p3_toolbar);
+            Toolbar tb=findViewById(R.id.tb1);
+            setSupportActionBar(tb);
+            ListView lv1 = findViewById(R.id.lv1);
+            String course[]={"Physics","Physique","Philosophy" ,"Chemistry", "Maths"};
+            ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1,course);
+            lv1.setAdapter(ad);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_p3_toolbar);
-        Toolbar tb= findViewById(R.id.tb1);
-        setActionBar(tb);
+
+
+        }
+
+
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.mymenu, menu);
+            MenuItem mSearch = menu.findItem(R.id.search_bar);
+            SearchView msv = (SearchView)mSearch.getActionView();
+            msv.setQueryHint("Enter a course");
+            msv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    ad.getFilter().filter(s);
+                    return true;
+                }
+            });
+
+            return super.onCreateOptionsMenu(menu);
+        }
+
+
 
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mymenu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-
-
-}
