cb27b1d975937c2b7b421b70e2119cecc83913f3
Arvind Kumar <arvind.11810214@lpu.in>
2021-04-03 23:05:03 +0530

    working on searchview

--- CA2Employee/app/src/main/java/com/example/ca2employee/MainActivity.java
+++ CA2Employee/app/src/main/java/com/example/ca2employee/MainActivity.java
@@ -1,20 +1,41 @@
 package com.example.ca2employee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         if (savedInstanceState!=null) return;
 
         // it will add the first fragment in the FrameLayout by default
         getSupportFragmentManager().beginTransaction().add(R.id.fl_main, new EmployeeListFragment()).commit();
 
+
+
+
+    }
+
+
+
+
+
+    public void showToast(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
