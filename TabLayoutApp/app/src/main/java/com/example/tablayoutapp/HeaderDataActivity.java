7479c70eef541e20daa55b2471071160270eab78
Arvind Kumar <arvind.11810214@lpu.in>
2021-04-27 22:19:30 +0530

    working on navigation drawer

---
+++ TabLayoutApp/app/src/main/java/com/example/tablayoutapp/HeaderDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.tablayoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeaderDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_header_data);
+    }
+}
--- TabLayoutApp/app/src/main/java/com/example/tablayoutapp/NavigationDrawerActivity.java
+++ TabLayoutApp/app/src/main/java/com/example/tablayoutapp/NavigationDrawerActivity.java
@@ -1,12 +1,14 @@
 package com.example.tablayoutapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
@@ -14,16 +16,22 @@ import com.google.android.material.navigation.NavigationView;
 public class NavigationDrawerActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, 0);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
         getDrawer();
     }
 
     public void getDrawer(){
-        drawerLayout = findViewById(R.id.drawerLayout);
+
         NavigationView navigationView = findViewById(R.id.navigationView);
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -45,12 +53,20 @@ public class NavigationDrawerActivity extends AppCompatActivity {
                 }
                 if (fragment!=null){
                     FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction.replace(R.id.fl_dl, fragment);
+                    fragmentTransaction.replace(R.id.fl_dl, fragment).commit();
+                    drawerLayout.closeDrawers();
                     return true;
                 }
 
-                return true;
+                return false;
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) return true;
+        return super.onOptionsItemSelected(item);
     }
 }
